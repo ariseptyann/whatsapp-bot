4b8c7c4758c2a97f0b7b55e28085183bfd6404ef
ariseptyan <ariseptyan@gringgofoundation.id>
2020-10-05 12:58:08 +0700

    First Commit

---
+++ server.js
@@ -0,0 +1,37 @@
+var express     = require('express');
+var cors        = require('cors');
+var v1Routes    = require('./server/routes');
+const app       = express();
+
+
+const { PORT = 3000 } = process.env;
+
+app.use(cors());
+
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+
+app.use(express.json());
+app.use(v1Routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        errors: {
+            message: err.message
+        }
+    });
+});
+
+app.listen(PORT, () => console.log(`App Listening on port ${PORT}`));
+
+// export default app;
